03b851ac0b8190e84ae449a4b1814e36313c1fdb
Abiodun Jobi <105235984+jhorbee01@users.noreply.github.com>
2022-09-10 09:14:22 +0100

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,39 +1,23 @@
 #include <stdio.h>
-
-
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - Prints numbers between 0 to 9 and letters between a to f.
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
-
 {
+	int d;
+	char low;
 
-		int i;
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
+	putchar('\n');
 
-
-
-			for (i = 48; i < 58; i++)
-
-					{
-
-								putchar(i);
-
-									}
-
-				for (i = 97; i < 103; i++)
-
-						{
-
-									putchar(i);
-
-										}
-
-					putchar('\n');
-
-						return (0);
-
+	return (0);
 }
