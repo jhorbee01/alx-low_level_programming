920f2d9e50b5d80f9d3356a0ce5598ca0a3b0548
Abiodun Jobi <105235984+jhorbee01@users.noreply.github.com>
2022-09-15 22:10:37 +0100

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,47 +1,24 @@
 #include <stdio.h>
-
 /**
+ * main - main function
  *
- *  * main - main function
- *
- *   *
- *
- *    * Return: always returns 0
- *
- *     */
-
+ * Return: always returns 0
+ */
 int main(void)
-
 {
+	unsigned int i = 2;
+	unsigned long n = 612852475143;
 
-		unsigned int i = 2;
-
-			unsigned long n = 612852475143;
-
-
-
-				while (i != n)
-
-						{
-
-									if (n % i == 0)
-
-												{
-
-																n = n / i;
-
-																		} else
-
-																					{
-
-																									i++;
-
-																											}
-
-																			}
-
-					printf("%lu\n", n);
-
-						return (0);
-
+	while (i != n)
+	{
+		if (n % i == 0)
+		{
+			n = n / i;
+		} else
+		{
+			i++;
+		}
+	}
+	printf("%lu\n", n);
+	return (0);
 }
