36d9122a526138c6c7b37740466c607e42457ce8
Abiodun Jobi <105235984+jhorbee01@users.noreply.github.com>
2022-09-20 00:41:19 +0100

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,37 +1,19 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * _strlen - finds length of a given string
- *
- *   * @s: string
- *
- *    * Return: int length of s
- *
- *    */
-
-
+ * _strlen - finds length of a given string
+ * @s: string
+ * Return: int length of s
+*/
 
 int _strlen(char *s)
-
 {
+	int len = 0, i = 0;
 
-		int len = 0, i = 0;
-
-
-
-			while (s[i] != '\0')
-
-					{
-
-								i++;
-
-										len++;
-
-											}
-
-				return (len);
-
+	while (s[i] != '\0')
+	{
+		i++;
+		len++;
+	}
+	return (len);
 }
