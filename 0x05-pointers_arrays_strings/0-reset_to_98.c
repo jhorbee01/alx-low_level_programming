f5f0de6b25855b4f7566ff442184b8fd565813d7
Abiodun Jobi <105235984+jhorbee01@users.noreply.github.com>
2022-09-20 00:38:04 +0100

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,21 +1,11 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * reset_to_98 - reset a varibale value to 98
- *
- *   * @n: int  pointer
- *
- *   */
-
-
+ * reset_to_98 - reset a varibale value to 98
+ * @n: int  pointer
+*/
 
 void reset_to_98(int *n)
-
 {
-
-		*n = 98;
-
+	*n = 98;
 }
