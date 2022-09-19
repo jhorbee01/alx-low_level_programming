4884b2b9bb7c1c6ad9fba8ed9ef6634760755dd0
Abiodun Jobi <105235984+jhorbee01@users.noreply.github.com>
2022-09-20 00:44:39 +0100

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,59 +1,30 @@
 #include <stdio.h>
-
 #include "main.h"
-
 #include "2-strlen.c"
 
-
-
 /**
- *
- *  * rev_string - print string in rev direction
- *
- *   * @s: string
- *
- *   */
-
-
+ * rev_string - print string in rev direction
+ * @s: string
+*/
 
 void rev_string(char *s)
-
 {
+	int len = _strlen(s);
+	int i = 0, j = 0;
+	static char str[1000];
 
-		int len = _strlen(s);
+	len--;
 
-			int i = 0, j = 0;
-
-				static char str[1000];
-
-
-
-					len--;
-
-
-
-						while (len >= 0)
-
-								{
-
-											str[i] = s[len];
-
-													len--;
-
-															i++;
-
-																}
-
-							while (j <= i)
-
-									{
-
-												s[j] = str[j];
-
-														j++;
-
-															}
-
-
+	while (len >= 0)
+	{
+		str[i] = s[len];
+		len--;
+		i++;
+	}
+	while (j <= i)
+	{
+		s[j] = str[j];
+		j++;
+	}
 
 }
