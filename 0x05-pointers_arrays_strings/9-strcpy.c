900bcbdf082fc24508853dd74056a9443960f6e5
Abiodun Jobi <105235984+jhorbee01@users.noreply.github.com>
2022-09-20 00:48:59 +0100

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,41 +1,21 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * _strcpy - copy a string to another string
- *
- *   * @dest: char array string to copy to
- *
- *    * @src: char array source to copy from
- *
- *     * Return: pointer to dest
- *
- *     */
-
-
+ * _strcpy - copy a string to another string
+ * @dest: char array string to copy to
+ * @src: char array source to copy from
+ * Return: pointer to dest
+*/
 
 char *_strcpy(char *dest, char *src)
-
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			while (src[i] != '\0')
-
-					{
-
-								dest[i] = src[i];
-
-										i++;
-
-											}
-
-				dest[i] = src[i];
-
-					return (dest);
-
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	return (dest);
 }
