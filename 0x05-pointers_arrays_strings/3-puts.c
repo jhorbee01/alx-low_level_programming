eac5d3dc18644620fd4351464fea03beedcfa7fb
Abiodun Jobi <105235984+jhorbee01@users.noreply.github.com>
2022-09-20 00:41:59 +0100

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,37 +1,19 @@
 #include <stdio.h>
-
 #include "main.h"
 
-
-
 /**
- *
- *  * _puts - print string to stdout
- *
- *   * @str: string
- *
- *   */
-
-
+ * _puts - print string to stdout
+ * @str: string
+*/
 
 void _puts(char *str)
-
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			while (str[i] != '\0')
-
-					{
-
-								putchar(str[i]);
-
-										i++;
-
-											}
-
-				putchar(10);
-
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar(10);
 }
