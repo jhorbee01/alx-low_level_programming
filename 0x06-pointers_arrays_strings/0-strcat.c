e13bd384c7752326f678cad6d1f3a8af605ac3d6
Abiodun Jobi <105235984+jhorbee01@users.noreply.github.com>
2022-09-23 02:35:02 +0100

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,53 +1,27 @@
 #include "main.h"
-
 #include "string.h"
-
 #include "strlen.c"
 
-
-
 /**
- *
- * * _strcat - concate 2 string
- *
- * * @dest: string
- *
- * * @src: string
- *
- * * Return: dest
- *
- * */
-
-
+* _strcat - concate 2 string
+* @dest: string
+* @src: string
+* Return: dest
+*/
 
 char *_strcat(char *dest, char *src)
-
 {
+	int i = 0;
+	int len1 = _strlen(dest);
+	int len2 = _strlen(src);
+	int nlen = len1 + len2;
 
-		int i = 0;
-
-			int len1 = _strlen(dest);
-
-				int len2 = _strlen(src);
-
-					int nlen = len1 + len2;
-
-
-
-						while (src[i] != EOS)
-
-								{
-
-											dest[len1 + i] = src[i];
-
-													i++;
-
-														}
-
-							nlen++;
-
-								dest[nlen] = EOS;
-
-									return (dest);
-
+	while (src[i] != EOS)
+	{
+		dest[len1 + i] = src[i];
+		i++;
+	}
+	nlen++;
+	dest[nlen] = EOS;
+	return (dest);
 }
