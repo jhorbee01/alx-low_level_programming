21a3760f15bdf0f1459edd75ef1b6dc1159f3874
Abiodun Jobi <105235984+jhorbee01@users.noreply.github.com>
2022-09-23 02:38:54 +0100

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,45 +1,23 @@
 #include "main.h"
 
-
-
 /**
- *
- * * reverse_array - compare 2 string
- *
- * * @a: int array
- *
- * * @n: int length of a
- *
- * */
-
-
+* reverse_array - compare 2 string
+* @a: int array
+* @n: int length of a
+*/
 
 void reverse_array(int *a, int n)
-
 {
+	int i = 0, tmp[100];
 
-		int i = 0, tmp[100];
-
-
-
-			n--;
-
-				for (i = n; i >= 0; i--)
-
-						{
-
-									tmp[i] = a[n - i];
-
-										}
-
-					for (i = 0; i <= n; i++)
-
-							{
-
-										a[i] = tmp[i];
-
-											}
-
-
+	n--;
+	for (i = n; i >= 0; i--)
+	{
+		tmp[i] = a[n - i];
+	}
+	for (i = 0; i <= n; i++)
+	{
+		a[i] = tmp[i];
+	}
 
 }
