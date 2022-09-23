ce4efc83193b2eb4411b97b1e498051ec2761f39
Abiodun Jobi <105235984+jhorbee01@users.noreply.github.com>
2022-09-23 02:48:47 +0100

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,29 +1,18 @@
 fndef MAIN_H
-
 #define MAIN_H
-
 char *_strcat(char *dest, char *src);
-
 char *_strncat(char *dest, char *src, int n);
-
 char *_strncpy(char *dest, char *src, int n);
-
 int _strlen(char *);
-
 int _strcmp(char *, char *);
-
 void reverse_array(int *, int);
-
 char *string_toupper(char *);
-
 char *cap_string(char *);
-
 int is_seperator(char);
-
 char *leet(char *);
-
 char *replace(char *dest, char *chk, char rplc);
-
-
+char *rot13(char *s)
+void print_number(int n)
+int main(void)
 
 #endif
