6315d2d0a110dee571ef22cd32b510fe5909d916
jhorbee01 <jhorbee01@gmail.com>
2022-09-22 18:33:54 -0700

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+#include "string.h"
+
+#include "strlen.c"
+
+/**
+ *
+ * * _strncat - concate 2 string
+ *
+ * * @dest: string
+ *
+ * * @src: string
+ *
+ * * @n: number of characters to append to dest
+ *
+ * * Return: dest
+ *
+ * */
+
+
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+
+		int i = 0;
+
+			int len1 = _strlen(dest);
+
+				int nlen = len1 + n;
+
+
+
+					while (src[i] != EOS)
+
+							{
+
+										if (i >= nlen)
+
+														break;
+
+												dest[len1 + i] = src[i];
+
+														i++;
+
+															}
+
+						nlen++;
+
+							dest[nlen] = EOS;
+
+								return (dest);
+
+}
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,89 @@
+#include "main.h"
+
+#include "strlen.c"
+
+/**
+ *
+ * * cap_string - capitallize each word of a string
+ *
+ * * @s: char array
+ *
+ * * Return: char array
+ *
+ * */
+
+
+
+char *cap_string(char *s)
+
+{
+
+		int i = 0, len = _strlen(s);
+
+
+
+			for (i = 0; i <= len; i++)
+
+					{
+
+								if (is_seperator(s[i]))
+
+											{
+
+															if ((s[i + 1] >= 97) && (s[i + 1] <= 122))
+
+																			{
+
+																								s[i + 1] = s[i + 1] - 32;
+
+																											}
+
+																	}
+
+									}
+
+				return (s);
+
+}
+
+
+
+/**
+ *
+ * * is_seperator - check whether a given character is a
+ *
+ * * a word seperator character
+ *
+ * * @c: char
+ *
+ * * Return: 1 if c is a word seperator else 0
+ *
+ * */
+
+
+
+int is_seperator(char c)
+
+{
+
+		char seperators[] = {10, 9, 12, 13, 32, 33,
+
+				34, 46, 59, 63, 40, 41, 123, 125, 44};
+
+			int i;
+
+
+
+				for (i = 0; i < 15; i++)
+
+						{
+
+									if (c == seperators[i])
+
+													return (1);
+
+										}
+
+					return (0);
+
+}
