d293b6d676943ec469793869cbd2c2d2f282a4c8
Abiodun Jobi <105235984+jhorbee01@users.noreply.github.com>
2022-09-23 02:38:03 +0100

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,57 +1,29 @@
 #include "main.h"
-
 #include "string.h"
-
 #include "strlen.c"
 
-
-
 /**
- *
- * * _strcmp - compare 2 string
- *
- * * @s1: string
- *
- * * @s2: string
- *
- * * Return: int
- *
- * */
-
-
+* _strcmp - compare 2 string
+* @s1: string
+* @s2: string
+* Return: int
+*/
 
 int _strcmp(char *s1, char *s2)
-
 {
+	int len1 = _strlen(s1), len2 = _strlen(s2);
+	int dff = len1 - len2, i = 0, result = 0;
 
-		int len1 = _strlen(s1), len2 = _strlen(s2);
-
-			int dff = len1 - len2, i = 0, result = 0;
-
-
-
-				if (dff < 0)
-
-							dff *= -1;
-
-					len1 += dff;
-
-						for (i = 0; i < len1; i++)
-
-								{
-
-											if (s1[i] - s2[i] != 0)
-
-														{
-
-																		result = s1[i] - s2[i];
-
-																					break;
-
-																							}
-
-												}
-
-							return (result);
-
+	if (dff < 0)
+		dff *= -1;
+	len1 += dff;
+	for (i = 0; i < len1; i++)
+	{
+		if (s1[i] - s2[i] != 0)
+		{
+			result = s1[i] - s2[i];
+			break;
+		}
+	}
+	return (result);
 }
