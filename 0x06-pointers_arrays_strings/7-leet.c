6ac733f6fd5f43c79b6ae07ab065ac9227deb3fc
Abiodun Jobi <105235984+jhorbee01@users.noreply.github.com>
2022-09-23 02:41:56 +0100

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,87 +1,44 @@
 #include "main.h"
-
 #include "string.h"
 
-
-
 /**
- *
- * * leet - encode some characters
- *
- * * @s: char array
- *
- * * Return: s encoded
- *
- * */
-
-
+* leet - encode some characters
+* @s: char array
+* Return: s encoded
+*/
 
 char *leet(char *s)
-
 {
-
-		replace(s, "aA", 52);
-
-			replace(s, "eE", 51);
-
-				replace(s, "oO", 48);
-
-					replace(s, "tT", 55);
-
-						replace(s, "lL", 49);
-
-							return (s);
-
+	replace(s, "aA", 52);
+	replace(s, "eE", 51);
+	replace(s, "oO", 48);
+	replace(s, "tT", 55);
+	replace(s, "lL", 49);
+	return (s);
 }
 
-
-
 /**
- *
- * * replace - replace some characters in a given string
- *
- * * @dest: char array to work on
- *
- * * @chk: char array to be replaced
- *
- * * @rplc: char to replace any character in chk
- *
- * * Return: dest encoded
- *
- * */
-
-
+* replace - replace some characters in a given string
+* @dest: char array to work on
+* @chk: char array to be replaced
+* @rplc: char to replace any character in chk
+* Return: dest encoded
+*/
 
 char *replace(char *dest, char *chk, char rplc)
-
 {
+	int i = 0, j = 0;
 
-		int i = 0, j = 0;
-
-
-
-			while (dest[i] != EOS)
-
-					{
-
-								j = 0;
-
-										while (chk[j] != EOS)
-
-													{
-
-																	if (dest[i] == chk[j])
-
-																						dest[i] = rplc;
-
-																				j++;
-
-																						}
-
-												i++;
-
-													}
-
-				return (dest);
-
+	while (dest[i] != EOS)
+	{
+		j = 0;
+		while (chk[j] != EOS)
+		{
+			if (dest[i] == chk[j])
+				dest[i] = rplc;
+			j++;
+		}
+		i++;
+	}
+	return (dest);
 }
